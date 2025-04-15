af772fb60027a88c016615b483eeeb2b87bdc1ee
ranselorm <ranselorm@outlook.com>
2025-04-15 17:46:40 +0000

    Serve frontend with static files

--- chapter_1/src/server.js
+++ chapter_1/src/server.js
@@ -2,35 +2,38 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const data = ["james"];
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello! Welcome to our backend system...");
+  res.sendFile("index.html", { root: "./public" });
 });
 
-app.get("/dashboard", (req, res) => {
-  console.log("Dashboard route accessed", req.method);
-  res.send("Hello! Welcome to our backend system...");
-});
+// app.get("/dashboard", (req, res) => {
+//   console.log("Dashboard route accessed", req.method);
+//   res.send("Hello! Welcome to our backend system...");
+// });
 
-app.get("/api/data", (req, res) => {
-  res.send(data);
-});
+// app.get("/api/data", (req, res) => {
+//   res.send(data);
+// });
 
-app.post("/api/data", (req, res) => {
-  const newEntry = req.body;
-  newEntry.name === "Randy"
-    ? data.push(newEntry.name)
-    : res.status(400).json({ message: "Name must be Randy" });
+// app.post("/api/data", (req, res) => {
+//   const newEntry = req.body;
+//   newEntry.name === "Randy"
+//     ? data.push(newEntry.name)
+//     : res.status(400).json({ message: "Name must be Randy" });
 
-  res.status(201).json({ message: "User created", data });
-});
+//   res.status(201).json({ message: "User created", data });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server has started on: ${PORT}`);
