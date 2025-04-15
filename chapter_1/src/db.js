6bc588bd28983d0b783c655a0d535722b56b18b3
ranselorm <ranselorm@outlook.com>
2025-04-15 21:05:44 +0000

    Add users and todos table

---
+++ chapter_1/src/db.js
@@ -0,0 +1,17 @@
+import { DatabaseSync } from "node:sqlite";
+const db = new DatabaseSync(":memory:");
+
+db.exec(`
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT
+)`);
+
+db.exec(`
+CREATE TABLE todos(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    task TEXT,
+    completed BOOLEAN
+)`);
