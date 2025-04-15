85164718cf2000927d82d34ec49ae9e1d1752547
ranselorm <ranselorm@outlook.com>
2025-04-15 21:09:13 +0000

    Indent tables

--- chapter_1/src/db.js
+++ chapter_1/src/db.js
@@ -2,16 +2,19 @@ import { DatabaseSync } from "node:sqlite";
 const db = new DatabaseSync(":memory:");
 
 db.exec(`
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE,
-    password TEXT
-)`);
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+`);
 
 db.exec(`
-CREATE TABLE todos(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    task TEXT,
-    completed BOOLEAN
-)`);
+    CREATE TABLE todos(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        task TEXT,
+        completed BOOLEAN DEFAULT 0,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+`);
